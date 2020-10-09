bd846dd54cb7408ae82c5cee56382de451dcc29c
Alessio Deiana <adeiana@gmail.com>
2020-10-09 11:39:01 -0500

    Removes py2 compatibility marking in setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
-#!/usr/bin/python
-# -*- coding:Utf-8 -*-
-
 from setuptools import setup
 
 try:
     from pypandoc import convert_file
-    read_md = lambda f: convert_file(f, 'rst')
+
+    def read_md(f):
+        return convert_file(f, 'rst')
 except ImportError:
     print("warning: pypandoc module not found, could not convert Markdown to RST")
-    read_md = lambda f: open(f, 'r').read()
+
+    def read_md(f):
+        return open(f, 'r').read()
 
 
 setup(name='redbaron',
       version='0.9.2',
-      description='Abstraction on top of baron, a FST for python to make writing refactoring code a realistic task',
+      description='Abstraction on top of baron, a FST for python to make '
+                  'writing refactoring code a realistic task',
       author='Laurent Peuch',
       long_description=read_md("README.md") + "\n\n" + open("CHANGELOG", "r").read(),
       author_email='cortex@worlddomination.be',
@@ -27,14 +29,8 @@ setup(name='redbaron',
       keywords='baron fst ast refactoring',
       classifiers=['Development Status :: 3 - Alpha',
                    'Intended Audience :: Developers',
-                   'Programming Language :: Python :: 2',
-                   'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
-                   'Programming Language :: Python :: 3.3',
-                   'Programming Language :: Python :: 3.4',
                    'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
                    'Topic :: Software Development',
                    'Topic :: Software Development :: Code Generators',
-                   'Topic :: Software Development :: Libraries',
-                   ],
-      )
+                   'Topic :: Software Development :: Libraries'])
