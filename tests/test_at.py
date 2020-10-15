25401a3f0a54d69f8f505e65f08292b7822e8f20
Alessio Deiana <adeiana@gmail.com>
2020-10-14 23:34:20 -0500

    Split at tests

--- tests/test_at.py
+++ tests/test_at.py
@@ -18,13 +18,41 @@ setup(name='redbaron',
 """)
 
 
-def test_at():
-    assert red.at(1) is red
+def test_at_root():
+    assert red.at(0) is red
+
+
+def test_at_1():
+    assert red.at(1) is red.find('ClassNode')
+
+
+def test_at_2():
     assert red.at(2) is red.find_all('DefNode')[0]
+
+
+def test_at_3():
     assert red.at(3) is red.find('AssignmentNode')
+
+
+def test_at_4():
     assert red.at(4) is red.find_all('DefNode')[1]
+
+
+def test_at_5():
     assert red.at(5) is red.find('ForNode')
+
+
+def test_at_6():
     assert red.at(6) is red.find('YieldNode')
+
+
+def test_at_7():
     assert red.at(7) is red.find_all('EndlNode')[6]
+
+
+def test_at_8():
     assert red.at(8) is red.find_all('AtomTrailersNode')[3]
+
+
+def test_at_9():
     assert red.at(9) is red.find_all('CallArgumentNode')[2]
